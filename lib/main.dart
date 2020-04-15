b397fba113aced18ac86ad3a348c2e19c4b3ebbd
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-04-15 14:08:13 +0300

    Random generator

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -32,7 +33,7 @@ class _DicePageState extends State<DicePage> {
             child: FlatButton(
               onPressed: () {
                 setState(() {
-                  leftDiceNumber = 5;
+                  leftDiceNumber = 1 + Random().nextInt(6);
                 });
                 print('left button got pressed.');
               },
